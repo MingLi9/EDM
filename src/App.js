c59e0d352667117737878e5cc18e5b7d08985ab8
MingLi9 <mingjanssen9@gmail.com>
2023-10-08 23:03:44 +0200

    Setup
    Basic setup with routs for the navigation.  Actual data for on the pages need to be places with css/javascript

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+        <Routes>
+        </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a class="nav-link" href="/">Datapunten overzicht</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="/onderwijsbiografie">Onderwijsbiografie</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
