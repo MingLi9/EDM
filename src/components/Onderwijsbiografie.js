1c12fd9580786cea396f3fd08d905701b389cb2b
MingLi9 <mingjanssen9@gmail.com>
2023-10-11 11:54:20 +0200

    Grammar correction

--- src/components/Onderwijsbiografie.js
+++ src/components/Onderwijsbiografie.js
@@ -25,9 +25,9 @@ export default function Onderwijsbiografie(){
                 Om een korte introductie te geven van dit hoofdstuk zal ik beginnen met hoe ik tot deze uitkomst ben gekomen.
                 De meeste mensen die ik ken laten normen aan waarden altijd in 1 zin vallen waardoor het niet altijd duidelijk
                 kan zijn wat ze belangrijk vinden en wat ze nu echt bedoelen. Na goed na te denken heb ik kunnen concluderen dat
-                Ik ben opgegroeid met eigenlijk maar 1 echte norm: Waarden zijn belangrijker dan normen.<br/>
+                Ik ben opgegroeid met maar 1 echte norm: Waarden zijn belangrijker dan normen.<br/>
                 Zoals ik het zie Zijn normen manieren waarop je waarden kunt uiten. Zo kan je bijvoorbeeld respect aantonen door u te zeggen i.p.v. je of jij.
-                Met deze gedachtengang heb ik er voor gekozen om normen verder niet meer centraal te zetten en te focussen op waarden en overtuigingen.
+                Met deze gedachtegang heb ik er voor gekozen om normen verder niet meer centraal te zetten en te focussen op waarden en overtuigingen.
                 Hieronder volg daarom een lijst waarin de waarden die ik het meest belangrijk vind zullen staan. Hierbij heb ik ook een beschrijving gegeven. 
                 Deze wordt getoond wanneer er op de balk van de waarde wordt geklikt. Hierin kunt u lezen welke situatie mij deze waarde heeft geleerd of juist
                 getoond heeft waarom deze waarde zo belangrijk is voor mij en/of voor mij om door te geven aan leerlingen/naasten.
